6ca21bd1742159f47f303d122bea4fbd87b3d722
xingcdev <60323726+xingcdev@users.noreply.github.com>
2024-02-15 20:38:44 +0100

    feat: add log message

--- src/main/java/com/efrei/st2dce/MyController.java
+++ src/main/java/com/efrei/st2dce/MyController.java
@@ -1,5 +1,7 @@
 package com.efrei.st2dce;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +10,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/")
 public class MyController {
 
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
     @GetMapping
     public String getGreetings() {
+        logger.debug("GET / is running successfully.");
         return "Hello, from ADXA (Antoine, Daniel, Xing, Aliaa) for Devops and Continuous Deployment final project.";
     }
 }
