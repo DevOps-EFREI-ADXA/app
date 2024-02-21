267193665902c824c050417491b8b2936b513106
Daniel <daniel.aguiar@outlook.fr>
2024-02-21 17:06:01 +0100

    debug: deployment file

--- scripts/deploy-infrastructure.sh
+++ scripts/deploy-infrastructure.sh
@@ -12,4 +12,6 @@ minikube kubectl -- config set-context --current --namespace=$env
 
 sed -i 's/<VERSION>/${versionNumber}/g' k8s/k8s-dev.yaml
 
+cat k8s/k8s-dev.yaml
+
 minikube kubectl -- apply -f k8s/k8s-dev.yaml
