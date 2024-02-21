3c804e2e169b5e5ce31a47d20f045d197964c79f
Daniel <daniel.aguiar@outlook.fr>
2024-02-21 17:01:34 +0100

    feat: update return message

--- src/main/java/com/efrei/st2dce/MyController.java
+++ src/main/java/com/efrei/st2dce/MyController.java
@@ -1,7 +1,5 @@
 package com.efrei.st2dce;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,11 +8,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/")
 public class MyController {
 
-    private final Logger logger = LoggerFactory.getLogger(this.getClass());
-
     @GetMapping
     public String getGreetings() {
-        logger.debug("GET / is running successfully.");
-        return "Hello, from ADXA (Antoine, Daniel, Xing, Aliaa) for Devops and Continuous Deployment final project for testing purposes.";
+        return "Hello, from ADXA (Antoine, Daniel, Xing, Aliaa) for Devops and Continuous Deployment final project v1";
     }
 }
